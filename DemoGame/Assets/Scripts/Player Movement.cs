28de385474f34ea1ac60c9b2c29b3f0c5be7e07c
GBFilipe <jagosti2@student.fitchburgstate.edu>
2024-02-05 15:46:53 -0800

    Added things

---
+++ DemoGame/Assets/Scripts/Player Movement.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float movementSpeed = 6f;
+    [SerializeField]float jumpForce = 5f;
+    public float jumpCooldown = 1.0f;
+    private float nextJumpTime = 0f;
+    private bool isJumping = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       float horizontalInput = Input.GetAxis("Horizontal");
+       float verticalInput = Input.GetAxis("Vertical");
+       
+       rb.velocity = new Vector3(horizontalInput * movementSpeed, rb.velocity.y, verticalInput * movementSpeed);
+       
+       if (Input.GetButtonDown("Jump"))
+       {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+       }
+
+       if (Input.GetButtonDown("Jump") && Time.time >= nextJumpTime)
+       {
+        Jump();
+        nextJumpTime = Time.time + jumpCooldown;
+       }
+    }
+    void Jump()
+    {
+        isJumping = true;
+
+        nextJumpTime = Time.time + jumpCooldown;
+    }
+
+    void EndJump()
+    {
+        isJumping = false;
+    }
+}
